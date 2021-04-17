83a5835759c2c76eca379c181a3cf086366e8394
taha-arslan <76772798+taha-arslan@users.noreply.github.com>
2021-04-18 00:33:41 +0300

    Update zoombot.py

--- zoombot.py
+++ zoombot.py
@@ -8,7 +8,7 @@ from datetime import datetime
 def join(id, password):
     subprocess.call(APPDATA + '\\Zoom\\bin\\Zoom.exe')
     while True:
-        time.sleep(7)
+        time.sleep(10)
         giriş = pyautogui.locateOnScreen('joinbuton.png')
         if giriş != None:
             pyautogui.click(giriş)
